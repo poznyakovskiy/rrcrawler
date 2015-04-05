867a428cda1b72eb6a74e95d475a212187ea2612
poznyakovskiy <anton.poznyakovskiy@gmail.com>
2015-04-05 23:36:52 +0200

    Added collecting data and saving it to a blob

--- rrcrawler.user.js
+++ rrcrawler.user.js
@@ -5,10 +5,40 @@
 // @include     http://rivalregions.com/#storage/listed*
 // @version     1
 // @grant       none
+// @require     https://raw.githubusercontent.com/eligrey/FileSaver.js/master/FileSaver.min.js
 // ==/UserScript==
 
 function collectDataAction (event) {
-  alert ("Stub!");
+  var list = document.getElementById ("list_tbody").children;
+  var textBody = "";
+  var fileName = "";
+  var node;
+  var blob;
+  var i;
+  var j;
+  
+  for (i = 0; i < list.length; ++i) {
+    node = list[i];
+    
+    // fetch bidder's name
+    textBody += node.children[1].children[0].innerHTML + ",";
+    
+    // fetch location
+    textBody += node.children[1].children[2].innerHTML + ",";
+    
+    // fetch amount, distance, price and adjusted price
+    for (j = 2; j <= 5; ++j) {
+      textBody += node.children[j].getAttribute("rat");
+      if (j != 5) {
+        textBody += ",";
+      }
+    }
+    
+    // end line
+    textBody += "\n";
+  }
+  blob = new Blob([textBody], {type: "text/plain;charset=utf-16"});
+  saveAs(blob, "data.csv");
 }
 
 window.addEventListener('load', function() {
