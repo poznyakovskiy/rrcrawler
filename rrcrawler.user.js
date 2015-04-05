538876712e468db1869b5de572f4ca07366d0ac8
poznyakovskiy <anton.poznyakovskiy@gmail.com>
2015-04-06 00:30:38 +0200

    Added timestamps, added .gitignore

--- rrcrawler.user.js
+++ rrcrawler.user.js
@@ -8,15 +8,26 @@
 // @require     https://raw.githubusercontent.com/eligrey/FileSaver.js/master/FileSaver.min.js
 // ==/UserScript==
 
+// auxiliary function for getting strings with leading zeros
+// source: http://stackoverflow.com/questions/2998784/how-to-output-integers-with-leading-zeros-in-javascript
+function pad(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
 function collectDataAction (event) {
   var list = document.getElementById ("list_tbody").children;
   var textBody = "";
-  var fileName = "";
+  var fileName;
+  var dateTime;
   var node;
   var blob;
+  var url;
   var i;
   var j;
   
+  // create file content
   for (i = 0; i < list.length; ++i) {
     node = list[i];
     
@@ -37,8 +48,25 @@ function collectDataAction (event) {
     // end line
     textBody += "\n";
   }
+  
+  // create file name
+  // get resource id from url
+  url = window.location.href;
+  fileName = url.substr (url.lastIndexOf('/')+1, url.length-1) + "_";
+  
+  // get current date and time
+  dateTime = new Date ();
+  fileName += String(dateTime.getFullYear()).substr(2,3) +
+                pad(dateTime.getMonth()+1,2) +
+                pad(dateTime.getDate(),2) + "_" +
+                pad(dateTime.getHours(),2) + 
+                pad(dateTime.getMinutes(),2);
+  
+  // that's the format
+  fileName += ".csv";
+  
   blob = new Blob([textBody], {type: "text/plain;charset=utf-16"});
-  saveAs(blob, "data.csv");
+  saveAs(blob, fileName);
 }
 
 window.addEventListener('load', function() {
